eaa120b6b9204efb0a626131994b2a27bacf5f15
arpit <arpit@temi.ca>
2022-04-27 13:22:17 -0400

    Two way DataBinding with LiveData

--- app/src/main/java/ca/temi/livedata/MainViewModel.kt
+++ app/src/main/java/ca/temi/livedata/MainViewModel.kt
@@ -5,12 +5,12 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
-    private val factsLiveDataObject = MutableLiveData<String>("This is a fact")
+    val quoteLiveDataObject = MutableLiveData<String>("This is a fact")
 
     val quoteLiveData: LiveData<String>
-        get() = factsLiveDataObject
+        get() = quoteLiveDataObject
 
     fun updateQuote() {
-        factsLiveDataObject.value = "Another facts"
+        quoteLiveDataObject.value = "Another Quote"
     }
 }
