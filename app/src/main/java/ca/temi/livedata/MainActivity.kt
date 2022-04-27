dbf98be0ec69a9ccf2385c5ccef64ed36032bffc
arpit <arpit@temi.ca>
2022-04-27 12:42:22 -0400

    Initial commit

---
+++ app/src/main/java/ca/temi/livedata/MainActivity.kt
@@ -0,0 +1,34 @@
+package ca.temi.livedata
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+
+class MainActivity : AppCompatActivity() {
+    lateinit var mainViewModel: MainViewModel
+
+    private val factTextView: TextView
+        get() = findViewById(R.id.factsTextView)
+
+    private val btnUpdate: Button
+        get() = findViewById(R.id.btnUpdate)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        mainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        mainViewModel.factsLiveData.observe(this, Observer {
+            factTextView.text = it
+        })
+
+        btnUpdate.setOnClickListener {
+            mainViewModel.updateLiveData()
+        }
+
+    }
+}
---
+++ app/src/main/java/ca/temi/livedata/MainViewModel.kt
@@ -0,0 +1,16 @@
+package ca.temi.livedata
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    private val factsLiveDataObject = MutableLiveData<String>("This is a fact")
+
+    val factsLiveData: LiveData<String>
+        get() = factsLiveDataObject
+
+    fun updateLiveData() {
+        factsLiveDataObject.value = "Another facts"
+    }
+}
