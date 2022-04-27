3732b6f468350bcad50110c5b689ba1edc89f421
arpit <arpit@temi.ca>
2022-04-27 13:02:03 -0400

    Data Binding with LiveData

--- app/src/main/java/ca/temi/livedata/MainActivity.kt
+++ app/src/main/java/ca/temi/livedata/MainActivity.kt
@@ -4,31 +4,26 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import ca.temi.livedata.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var mainViewModel: MainViewModel
 
-    private val factTextView: TextView
-        get() = findViewById(R.id.factsTextView)
+    lateinit var binding: ActivityMainBinding
 
-    private val btnUpdate: Button
-        get() = findViewById(R.id.btnUpdate)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        mainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
 
-        mainViewModel.factsLiveData.observe(this, Observer {
-            factTextView.text = it
-        })
+        val quoteObj = Quote("Do or do not Ther is try","Yoda")
 
-        btnUpdate.setOnClickListener {
-            mainViewModel.updateLiveData()
-        }
+        binding.quote = quoteObj
+
 
     }
 }
